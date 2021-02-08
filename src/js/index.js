fbc54c1a913fb989f9ffc8c099d3a956218dfba4
Lucas Araujo <lukas.scaa@gmail.com>
2021-02-08 10:00:00 +0000

    Create Fullpage with sections

--- src/js/index.js
+++ src/js/index.js
@@ -1,11 +1,3 @@
-import "../sass/index.scss"
-
-
-const initHelloWorld = () =>{
-    const tagH1 = document.createElement('h1')
-    tagH1.innerHTML = 'Im Groot'
-    tagH1.classList.add('title')
-    return tagH1
-}
-
-document.body.appendChild(initHelloWorld())
+import '../js/fullpage'
+import '../sass/index.scss'
+import '../js/initComponents'
