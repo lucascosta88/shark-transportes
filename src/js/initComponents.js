09fc03c7fc0a6f5b1b0885cbb2371ae12be4348a
Lucas Araujo <lukas.scaa@gmail.com>
2021-02-08 10:08:10 +0000

    Fullpage inline html

--- src/js/initComponents.js
+++
@@ -1,7 +0,0 @@
-new fullpage('#fullPage', {
-    autoScrolling: true,
-    navigation: true,
-    anchors: ['section1', 'section2', 'section3','section4','section5','section6'],
-    navigationTooltips: ['Home', 'Rastreio', 'Cotação','Serviço de Envio','Licenças','Parceiros']
-
-})
